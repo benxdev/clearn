32efd3fde26ab0c477b0a458d507069b4878aa82
Benedict <83441061+benxdev@users.noreply.github.com>
2025-03-23 05:59:48 +0100

    Update cc4e.c

--- cc4e.c
+++ cc4e.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
-// min max
-int main () {
-    int first = 1;
-    int val, minval, maxval;
-
-    while (scanf("%d", &val) != EOF) {
-        if (first || val > maxval ) maxval = val;
-        if (first || val < minval ) minval = val;
-        first = 0;
-    }
-
-    printf("max value is %d\nmin value is %d\n", maxval, minval);
+int main() {
+    int mymult();
+    int output;
+    output = mymult(324, 598);
+    printf("answer is %d\n", output);
+   
 }
+
+ 
+    int mymult(int a, int b) {
+  
+        int c = a * b;
+        return c;
+     }
